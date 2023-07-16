c55f074a4aaf5fb0de09dd8ada1e3eddcc2ec2f2
Ayush-Vishwakarma007 <ayushvishwakarma767@gmail.com>
2023-07-16 16:21:34 +0530

    [AV][Home] - Added seats with actuall data of auditorium for testing purpose

--- src/app/auditorium-seats/auditorium-seats.component.ts
+++ src/app/auditorium-seats/auditorium-seats.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 interface Seat {
   row: string;
@@ -6,6 +8,7 @@ interface Seat {
   selected: boolean;
   reserved: boolean;
   booked: boolean;
+  handicap: boolean;
 }
 
 interface Row {
@@ -18,35 +21,52 @@ interface Row {
   templateUrl: './auditorium-seats.component.html',
   styleUrls: ['./auditorium-seats.component.scss']
 })
-export class AuditoriumSeatsComponent {
-  rows: Row[] = [
-    {
-      name: 'A',
-      seats: [
-        { row: 'A', number: 1, selected: false, reserved: false, booked: false },
-        { row: 'A', number: 2, selected: false, reserved: true, booked: false },
-        { row: 'A', number: 3, selected: false, reserved: false, booked: true },
-        // ... add more seat objects for row A
-      ]
-    },
-    {
-      name: 'B',
-      seats: [
-        { row: 'B', number: 1, selected: false, reserved: false, booked: true },
-        { row: 'B', number: 2, selected: false, reserved: false, booked: false },
-        { row: 'B', number: 3, selected: false, reserved: false, booked: true },
-        // ... add more seat objects for row B
-      ]
-    },
-    // ... add more row objects
-  ];
+export class AuditoriumSeatsComponent implements OnInit {
+  constructor(private http: HttpClient){}
+  seatLists: any[]=[]
+  rowLists: any[]=[]
   selectedSeats: Seat[] = [];
 
-  selectSeat(seat: Seat) {
-    if (!seat.reserved && !seat.booked) {
+  ngOnInit(): void {
+    this.getDataFromJSONFile().subscribe((response: any) => {
+      console.log(response['data']);
+      response['data'].forEach((data:any) => {
+        data['rowList'].forEach((rowList:any) => {
+          this.rowLists.push(rowList);
+        });
+      });
+      console.log("Seat List: ", this.rowLists)
+    });
+  }
+
+  // rows: Row[] = [
+  //   {
+  //     name: 'A',
+  //     seats: [
+  //       { row: 'A', number: 1, selected: false, reserved: false, booked: false, handicap: false },
+  //       { row: 'A', number: 2, selected: false, reserved: true, booked: false, handicap: false },
+  //       { row: 'A', number: 3, selected: false, reserved: true, booked: true, handicap: true },
+  //       // ... add more seat objects for row A
+  //     ]
+  //   },
+  //   {
+  //     name: 'B',
+  //     seats: [
+  //       { row: 'B', number: 1, selected: false, reserved: false, booked: true, handicap: false },
+  //       { row: 'B', number: 2, selected: false, reserved: false, booked: false, handicap: true },
+  //       { row: 'B', number: 3, selected: false, reserved: false, booked: true, handicap: false },
+  //       // ... add more seat objects for row B
+  //     ]
+  //   },
+  //   // ... add more row objects
+  // ];
+
+  selectSeat(seat: any) {
+    if (seat.seatStatus === 'AVAILABLE') {
       seat.selected = !seat.selected;
       if (seat.selected) {
         this.selectedSeats.push(seat);
+        console.log("Selected Seats: ", this.selectedSeats)
       } else {
         const index = this.selectedSeats.findIndex(s => s.row === seat.row && s.number === seat.number);
         if (index !== -1) {
@@ -55,5 +75,21 @@ export class AuditoriumSeatsComponent {
       }
     }
   }
+
+  // selectSeat(seat: any) {
+  //   if (seat.seatStatus === 'AVAILABLE') {
+  //     seat.selected = !seat.selected;
+  //     if (seat.selected) {
+  //       this.selectedSeats.push(seat)
+  //     } else {
+  //     }
+  //   }
+  // }
+
+
+  getDataFromJSONFile() {
+    return this.http.get('../../assets/seat_data.json');
+  }
+
 
 }
